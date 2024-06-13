1d7eb5a69a7473c408178250cfcd6b400f5abbc1
Bytefield <ramosperez1978@gmail.com>
2024-06-13 10:43:46 +0200

    --alllow-empty

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import carrito from './assets/carrito.png';
+
+function App() {
+
+    return (
+        <main className='app'>
+            <h4 className='success'>You are done</h4>
+            <div className="header">
+                <h1>Shopping List</h1>
+                <img src={carrito} alt="some img" />
+                <input type="text" placeholder="Add an item" />
+            </div>
+            <ul>
+                <li>
+                    <div className="container">
+                        <input type="checkbox" name="" id="" />
+                    </div>
+                </li>
+            </ul>
+        </main>
+    )
+}
+
+export default App
