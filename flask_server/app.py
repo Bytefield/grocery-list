11056087f8a6a1883a1634d6acd5c494a1a6667e
Bytefield <ramosperez1978@gmail.com>
2024-06-19 14:10:45 +0200

    tokenization succesful, response 200

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from transformers import BertTokenizer
 
 app = Flask(__name__)
 CORS(app)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-@app.route('/api/data')
+@app.route('/api/data', methods=['GET', 'POST'])
 def hello_world():
-    return jsonify(message="Hello, World!")
+    if request.method == 'POST':
+        data = request.get_json()
+        # Tokenize the data here
+        tokenized_data = tokenizer.tokenize(data['text'])
+        return jsonify(tokenized_data)
+    else:
+        return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
     app.run(debug=True)
