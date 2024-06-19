e80336d43fbc463273850831c24e159ba8888125
Bytefield <ramosperez1978@gmail.com>
2024-06-19 14:19:04 +0200

    made fetch function async
    created json with selenium actions in json format

--- src/App.jsx
+++ src/App.jsx
@@ -54,14 +54,21 @@ function App() {
 
     const fetch_data = () => {
         fetch('http://localhost:5000/api/data', {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
+            body: JSON.stringify({ text: 'The data you want to tokenize' }),
         })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch((error) => console.error('Error:', error));
+            .then(async response => {
+                if (!response.ok) {
+                    const text = await response.text();
+                    throw new Error(text);
+                }
+                return response.json();
+            })
+            .then(data => console.log(data))
+            .catch((error) => console.error('Error:', error));
     }
 
     return (
